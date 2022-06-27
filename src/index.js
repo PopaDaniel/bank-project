6313d909d1002555964d0118bc691539b6b8ec49
PopaDaniel <77845766+PopaDaniel@users.noreply.github.com>
2022-06-27 15:27:32 +0300

    make it better

--- src/index.js
+++ src/index.js
@@ -25,22 +25,21 @@ mongoose
   });
 app.use(cors());
 app.use(express.json());
-app.use("/api", adminRoutes);
 app.use("/api", useRoutes);
+app.use("/api", adminRoutes);
 
 // Serve the frontend
-app.use(express.static(path.join(__dirname, "../frontend/build")));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+  app.get("*", (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, "../", "frontend", "build", "index.html")
+    )
+  );
+} else {
+  app.get("/", (req, res) => res.send("Please set to production"));
+}
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
-});
-
-// app.use(express.static(path.join(__dirname, "../frontend/build")));
-// app.get("*", (req, res) =>
-//   res.sendFile(
-//     path.resolve(__dirname, "../", "frontend", "build", "index.html")
-//   )
-// );
 app.listen(process.env.PORT || 2000, () => {
   console.log(`Server is running `);
 });
